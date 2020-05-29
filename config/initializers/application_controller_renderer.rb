3ecca71094471f6b76dbc61dc45656588863218f
Kamitotomi <rockyaaokage@gmail.com>
2020-05-29 20:36:37 +0900

    [Finish] 5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '2a04199b43b7113bcc99472f4cbab32c75ba37653ed01459e742aff2f6c7aaa95370ddfabb3565631903fa98fb3afde79f02ac8883a403023d62325ea4f2f9c8'
